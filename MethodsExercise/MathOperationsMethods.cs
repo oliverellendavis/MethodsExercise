e603e8e2a4d7f7db318247c11611a3a50a65e006
oliverellendavis <oliverellendavis@gmail.com>
2024-09-29 14:19:08 -0400

    Completed Exercise 2 with math operations methods

---
+++ MethodsExercise/MathOperationsMethods.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodsExercise;
+
+public class MathOperationsMethods
+{
+    public void MathOperations()
+    {
+        Console.WriteLine(Add(3, 6)); // Output: 9
+        Console.WriteLine(Multiply(3, 6)); //Output: 18
+        Console.WriteLine(Subtract(6, 3)); //Output: 3
+        Console.WriteLine(Divide(6, 3)); //Output: 2
+    }
+
+    static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    static int Divide(int x, int y)
+    {
+        return x / y;
+    }
+}
+    
