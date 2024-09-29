a27b04c774240ce1d1dbde3f68512059dd381979
oliverellendavis <oliverellendavis@gmail.com>
2024-09-29 13:53:02 -0400

    Completed C# story program exercise

---
+++ MethodsExercise/StoryProgram.cs
@@ -0,0 +1,27 @@
+﻿namespace MethodsExercise;
+
+public class StoryProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("What is your favourite colour?");
+        string colour = Console.ReadLine();
+        
+        Console.WriteLine("What is your favourite animal?");
+        string animal = Console.ReadLine();
+        
+        Console.WriteLine("What is your favourite band?");
+        string band = Console.ReadLine();
+
+        string story = $@"
+        My cat's name is {name} and her fur is {colour}.
+        Her best friend is a {animal} and they like to listen to music together.
+        The band they like to listen to the most is {band}.
+        ";
+        
+        Console.WriteLine(story);
+    }
+}
